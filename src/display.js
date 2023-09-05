bfd6977d3aa983791eb3ad087aaa0948164d7f4d
nedimburo <nedimb9402@gmail.com>
2023-09-05 14:56:31 +0200

    Changed layout and added new icons

--- src/display.js
+++ src/display.js
@@ -1,37 +1,5 @@
 const displayController=(()=>{
-    let todoMainContainer=document.getElementById("todo-container");
-    let addButtonsContainer=document.getElementById("buttons-container");
-    let todoListing=document.getElementById("todo-listing");
-    const createInputBox=type=>{
-        addButtonsContainer.style.display="none";
-        let inputContainer=document.createElement("div");
-        inputContainer.setAttribute("id", "input-container");
-        let inputField=document.createElement("input");
-        inputField.setAttribute("id", "input-name-box");
-        let submitButton=document.createElement("button");
-        let cancelButton=document.createElement("button");
-        submitButton.classList.add("input-field-button");
-        cancelButton.classList.add("input-field-button");
-        submitButton.classList.add("submit");
-        cancelButton.classList.add("cancel");
-        submitButton.textContent="Submit";
-        cancelButton.textContent="Cancel";
-        (type=="task") ? inputField.placeholder="Enter task title..." 
-                        : inputField.placeholder="Enter project name...";
-        cancelButton.addEventListener("click", removeInputBox);
-        inputContainer.appendChild(inputField);
-        inputContainer.appendChild(submitButton);
-        inputContainer.appendChild(cancelButton);
-        todoMainContainer.appendChild(inputContainer);
-    };
-    const removeInputBox=()=>{
-        let inputContainer=document.getElementById("input-container");
-        todoMainContainer.removeChild(inputContainer);
-        addButtonsContainer.style.display="flex";
-    };
-    return{
-        createInputBox,
-    }
+    
 })();
 
 module.exports=displayController;
--- src/project.js
+++ src/project.js
@@ -1,9 +1,12 @@
 export default class Project{
     constructor(title){
         this.title=title;
+        this.projects=[];
     }
     
     get title(){ return this._title};
+    get projects(){ return this._projects};
 
     set title(value){ this._title=value};
+    set projects(value){ this._projects=value};
 }
